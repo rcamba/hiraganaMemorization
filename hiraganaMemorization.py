658c5e0a6f2ae9f7e13961ad71c7b49fab63adb8
Robert <robertCamba@gmail.com>
2015-06-02 20:36:22 -0500

    move dictionary to separate file handled mini-tsu

--- hiraganaMemorization.py
+++ hiraganaMemorization.py
@@ -8,12 +8,12 @@ from os import listdir, chdir, system, getcwd, path
 from random import randint, choice as randChoice
 from time import sleep
 from threading import Thread
-from sys import argv 
+from sys import argv
 
 from collections import OrderedDict
 
 """
-TODO: 
+TODO:
 	add scoring
 	create log to display which characters that I make mistakes on
 	input will now have to be separated in spaces to allow counting mistakes to create the log
@@ -21,145 +21,122 @@ TODO:
 
 
 
+def loadSymDicts(wD):
+	temp={}
+	targs=["verbs"]
+	for t in targs:
+		d=open("symDicts\\"+t).read().replace("\n","")
+		d=d.lower()
+		exec("temp="+"{"+d+"}")
+		wD.update(temp)
 
-wD={"ta-be-ma-su":"to eat", 
-"na-ra-i-ma-su":"to learn", 
-"wa-su-re-ma-su":"to forget", 
-"no-mi-ma-su":"to drink", 
-"ka-i-ma-su":"to buy", 
-"mi-ma-su":"to watch, look,see", 
-"mi-se-ma-su":"to show",
-"ka-ki-ma-su":"to write, draw, paint",
-"o-ku-ri-ma-su":"to send",
-"tsu-ku-ri-ma-su":"to make, produce, cook",
-"tsu-ka-i-ma-su":"to use",
-"i-ki-ma-su":"to go",
-"ki-ma-su":"to come",
-"ka-e-ri-ma-su":"to return",
-"a-ri-ma-su":"to have, be at, exist(inanimate object)",
-"i-ma-su":"to have, be at, exist(animate object)",
-"ha-na-shi-ma-su":"to talk, speak",
-"ya-ku-shi-ma-su":"to translate",
-"ne-ma-su":"to lie down, go to bed",
-"o-ki-ma-su":"to get up, wake up, happen, occur",
-"ko-wa-re-ma-su":"to be broken",
-"na-o-shi-ma-su":"to repair, fix",
-"a-ge-ma-su":"to give, present/ to raise, lift up",
-"mo-ra-i-ma-su":"to receive, be given",
-"ka-ri-ma-su":"to borrow, rent",
-"a-ga-ri-ma-su":"to go up, rise",
-"sa-ga-ri-ma-su":"to go down, drop",
-"be-n-kyo-o": "to learn, study",
-"cho-o-sa":	"to investigate, inquire",
-"chu-u-mo-n": "to order (goods)",
-"de-n-wa": "to phone, call",
-"ge-n-sho-o":  "to decrease, drop",
-"ha-n-ta-i": "to oppose, be against",
-"ha-tsu-ba-i": "to release, be on sale",
-"he-n-ji": "to reply, answer",
-"he-n-ka":	"to change",
-"ho-n-ya-ku": "to translate",
-"ho-o-ko-ku": "to report",
-"i-n-sa-tsu": "to print",
-"ka-i-ha-tsu": "to develop",
-"ka-ku-ni-n": "to check, confirm",
-"ka-n-ri": "to manage, control",
-"ka-n-se-i": "to complete",
-"ke-i-sa-n": "to calculate",
-"ke-n-kyu-u": "to study, research",
-"ke-n-sa": "to check, test, inspect",
-"ke-n-sa-ku": "to search",
-"ke-n-to-o": "to discuss, study, think over",
-"ki-ko-ku" : "to return to one's country"
-}
 
+
+
+wD={}
+loadSymDicts(wD)
 
 class ErrorList:
-	
+
 	def __init__(self, characterEr, erCount):
 		self.characterError=characterEr
 		self.count=int(erCount)
-	
-	@staticmethod	
+
+	@staticmethod
 	def load(errorLogFile):
-		
+
 		errorList=[]
 		dataError=open(errorLogFile).read().split('\n')
-		
+
 		for data in dataError:
-			
+
 			dataSplit=data.split(':')
 			if (len(dataSplit)>1 ):
 				errorList.append(ErrorList(dataSplit[0],dataSplit[1]))
-			
+
 		return errorList
-	
-	@staticmethod	
+
+	@staticmethod
 	def writeLog(errorLogFile, errorList):
 		#writes errorList to errorLog
 		writer=open(errorLogFile,'w')
 		for err in errorList:
-			
+
 			writer.write("".join([err.getCharacter(),':',str(err.getCount())]))
 			writer.write('\n')
 		writer.close()
-		
+
 	def getCharacter(self):
 		return self.characterError
-		
+
 	def getCount(self):
 		return self.count
-		
+
 	def increaseCount(self):
 		self.count=int(self.count)+1
-			
+
 	def __str__(self):
 		return "".join([ self.characterError, ':', str(self.count)])
 
 def createLog(userAns):
-	
+
 	uAnsList=userAns.split()
 	cAnsList=correctAns.split()
-	
+
 	for i in range(0,len(uAnsList)):
 		if(uAnsList[i].lower()!=cAnsList[i].lower()):
 			increaseErrorCount(cAnsList[i])
-			
+
 def increaseErrorCount(error):
 	#finds the error in the log and increases it by 1
 	eList=ErrorList.load(mistakesLog)
-	
+
 	for e in eList:
 		if(e.getCharacter().lower()==error.lower()):
 			e.increaseCount()
-			
+
 			break
-	
+
 	ErrorList.writeLog(mistakesLog, eList)
 
+def generateCorrectAns(tokens):
+	correctAns=""
+	vowels=['a','e','o','i','u']
+	for i in range(0,len(tokens)):
+		
+		if tokens[i]=="minitsu":
+			if i<len(tokens)-1 and tokens[i+1][0] not in vowels:
+				correctAns=correctAns+tokens[i+1][0]
+			else:
+				raise ValueError("Last syllable can't be a Sokuon(little tsu) and must not be a vowel" + ". Tokens="+ str(tokens))
+		else:
+			correctAns=correctAns+tokens[i]
+			
+	return correctAns
+			
 prevCanvas=None
 deletePrev=False
 def handleUserInput(master, canvas, textFrame, inputBox, inputLabel, imgFileList, wordFileDict):
-	
+
 	global prevCanvas
 	global canvasCount
 	global deletePrev
-	
+
 	userInput=inputBox.get()
-	
-	if userInput!="quit":	
-		
-		
-		correctAns= "".join(  tokens )
+
+	if userInput!="quit":
+
+
+		correctAns= generateCorrectAns(  tokens )
 		if correctAns==userInput:
-			print "Correct!"
-		
-		
+			print userInput, "is correct!"
+
+
 		else:
-			print "Wrong. Answer is: ", correctAns
-		
-		
-		
+			print userInput, "is wrong! Answer is: ", correctAns
+
+
+
 		if deletePrev==False:
 			deletePrev=True
 			if prevCanvas!=None:
@@ -167,82 +144,85 @@ def handleUserInput(master, canvas, textFrame, inputBox, inputLabel, imgFileList
 				canvas.destroy()
 			else:
 				prevCanvas=canvas
-			
-		
+
+
 		elif deletePrev==True:
 			prevCanvas.destroy()
 			prevCanvas=canvas
-			
+
 		textFrame.destroy()
-		
+
 		inputLabel.destroy()
 		inputBox.destroy()
-		
-		createWord(imgFileList,wordFileDict)
-		
-		canvas=drawSyllables(master, canvas, wordFileDict)
-		(inputLabel, inputBox)=drawInputBox(master,  canvas, textFrame, inputBox, inputLabel, imgFileList, wordFileDict)
-		
-		userInput=inputBox.get()
+		if len(wD)>0:
+			createWord(imgFileList,wordFileDict)
+
+			canvas=drawSyllables(master, canvas, wordFileDict)
+			(inputLabel, inputBox)=drawInputBox(master,  canvas, textFrame, inputBox, inputLabel, imgFileList, wordFileDict)
+
+			userInput=inputBox.get()
 	
+		else:
+			master.destroy()
+
 	else:
 		master.destroy()
-		
+
 tokens=[]
 def createWord(imgFileList, wordFileDict):
-	
+
 	wordFileDict.clear()
 	global tokens
 	i=0
 	word=""
-	
-	
+
+
 	syllables=randChoice(wD.keys())
 	tokens=syllables.split('-')
 	for t in tokens:
 		wordFileDict[ t ] = t+".png"
-	
-	
-	
+
+
+
 imgHolder=[]
 def drawSyllables(master, canvas, wordFileDict):
 	global imgHolder
-	
-	
-	
+
+
+
 	startWidth=0
 	canvas_width=len(tokens)*100
 	canvas_height=100
-	
+
 	canvas = Canvas(master, width=canvas_width, height=canvas_height)
 	for t in tokens:
 		fileName= t+".png"
 		filePath=SYLLABLE_LIST_DIR+"\\"+ fileName
-		imageFile=Image.open( filePath ) 
+		imageFile=Image.open( filePath )
 		imgHolder.append(  ImageTk.PhotoImage( imageFile) )
 		#store because PhotoImage copy issues; "wrapper for their copy() method is botched"
-		
+
 		canvas.create_image(startWidth, 0, anchor=NW,  image=imgHolder[len(imgHolder)-1])
-		
+
 		startWidth+=imageFile.size[0]
-		
+
 	canvas.pack()
-	
+
 	return canvas
 
 
 def drawInputBox(master, canvas, textFrame, inputBox, inputLabel,  imgFileList, wordFileDict):
-	
+
 	#Create a Label in textFrame
-	
+
 	textFrame= Frame(master)
-	
-	
+
+
 	wordLabel=Label(textFrame)
 	wordLabel["text"]=wD[ "-".join( tokens ) ]
 	wD.pop(  "-".join( tokens ) , None)
 	wordLabel.pack()
-	
+
 	inputLabel = Label(textFrame)
 	inputLabel["text"] = "Enter syllables:"
 	inputLabel.pack(side=LEFT)
@@ -251,77 +231,75 @@ def drawInputBox(master, canvas, textFrame, inputBox, inputLabel,  imgFileList, 
 	inputBox = Entry(textFrame)
 	inputBox["width"] = 50
 	inputBox.pack(side=LEFT)
-	
+
 	inputBox.focus_force()
 
 	inputBox.bind("<Return>", lambda func:handleUserInput(master, canvas, textFrame,  inputBox, inputLabel, imgFileList, wordFileDict) )
-	
-	
-	
+
+
+
 	inputBox.pack()
 	textFrame.pack()
-	
+
 	return (inputLabel, inputBox)
 
 
 def createHistogram():
 	erList=ErrorList.load(mistakesLog)
-	
+
 	#count all errors then make chart
 	histogram=[]
 	numOfErrors=0
 	for e in erList:
-		
+
 		if ( e.getCount() >0):
 			numOfErrors=numOfErrors+1
 			histogram.append(e)
-	
+
 	#printing
 	for h in histogram:
 		print h.getCharacter(), "\t:\t", getNumOfBars(h.getCount()/(numOfErrors*1.0)), round((h.getCount()/((numOfErrors)*1.0))*100,2),"%"
-		
+
 def getNumOfBars(num):
 	result=""
-	
+
 	for i in range(0,int(num*100)):
 		result="".join([result,"[]"])
-	
+
 	return result
 
 def main():
-	
+
 	master = Tk()
-	
+
 	master.title("Hiragana")
 	master["padx"] = 20
-	master["pady"] = 20 
-	
+	master["pady"] = 20
+
 	wordFileDict={}
 	wordFileDict=OrderedDict(wordFileDict)
 	textFrame=None
 	inputBox=None
 	inputLabel=None
-	
+
 	imgFileList=listdir(SYLLABLE_LIST_DIR)
 	createWord(imgFileList, wordFileDict)
-	
+
 	canvas=None
 	canvas=drawSyllables(master,  canvas, wordFileDict)
 	(inputLabel, inputBox)=drawInputBox(master,  canvas, textFrame, inputBox, inputLabel, imgFileList, wordFileDict)
-	
-	
+
+
 	mainloop()
-	
-	
+
+
 if __name__ == "__main__":
-	
+
 	SYLLABLE_LIST_DIR="symImg"
 	mistakesLog="mistakes.log"
 
-	
+
 	if ("p" in argv):
 		createHistogram()
 	else:
 		main()
-	
-	
