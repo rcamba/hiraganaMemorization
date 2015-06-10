9acdc8d34436bc2f14ed0e2690dc291b9747c95e
Robert <robertCamba@gmail.com>
2015-06-09 23:30:50 -0500

    mess that just prints out characters w/ centering and one test

--- hm_eventHandlers.py
+++ hm_eventHandlers.py
@@ -7,3 +7,37 @@ def hideRomajiHandler(obj):
 def handleClickInputBox(evt):
 	evt.GetEventObject().SetValue("")
 	evt.GetEventObject().SetFocus()
+
+
+def fileListForWord(word):
+	return [syllable.lower()+".png" for syllable in word.split('-')]
+
+def correctAnsForFileList(fileList):
+
+	word=""
+	vowels=['a','e','o','i','u']
+
+	for i in range (0,len(fileList)) :
+		fileList[i]=fileList[i].replace(".png","")
+
+		if fileList[i]=="minitsu":
+			if i<len(fileList)-1:
+				if fileList[i+1][0] not in vowels:
+					word=word+(fileList[i+1][0])
+				else:
+					raise ValueError("Last syllable must not be a vowel")
+			else:
+				raise ValueError("Last syllable can't be a Sokuon(little tsu)")
+
+		else:
+			word=word+(fileList[i])
+
+	return word
+
+def drawNextWord(wordDict):
+	randWord=randChoice(wordDict.keys())
+	wordDict.pop(randWord)
+	fileList=fileListForWord(randWord)
+
+	return fileList
+
--- wxHiraganaMemorization.py
+++ wxHiraganaMemorization.py
@@ -2,10 +2,13 @@ from ConfigParser import RawConfigParser
 from os import path
 import wx
 from hm_eventHandlers import *
+from random import choice as randChoice
 
 pathToModule=path.dirname(__file__)
 if len(pathToModule)==0:
 	pathToModule='.'
+
+
 
 class MainPanel(wx.Panel):
 
@@ -16,21 +19,23 @@ class MainPanel(wx.Panel):
 		#self.confParser=RawConfigParser()
 		#self.configFile=pathToModule+"/../config.conf"
 		#self.confParser.read(self.configFile)
-
-
+		self.imgBoxSize=(800,100)
+		self.symDictPath=path.join(pathToModule,"symDicts")
+		self.symImgPath=path.join(pathToModule,"symImg")
 
 		self.topSizer = wx.BoxSizer(wx.HORIZONTAL)
 		self.toggleOptSizer = wx.BoxSizer(wx.VERTICAL)
 
-		self.prevImgBoxSizer = wx.BoxSizer(wx.VERTICAL)
-		self.currImgBoxSizer= wx.BoxSizer(wx.VERTICAL)
+		self.prevImgBoxSizer = wx.BoxSizer(wx.HORIZONTAL)
+		self.currImgBoxSizer= wx.BoxSizer(wx.HORIZONTAL)
 		self.imgBoxSizer= wx.BoxSizer(wx.VERTICAL)
 
 		self.clickableOptSizer= wx.BoxSizer(wx.VERTICAL)
 
 		self.inputTxtSizer = wx.BoxSizer(wx.HORIZONTAL)
 
-
+		self.defaultDict=["verbs"]
+		self.wordDict=self.loadSymDicts()
 
 		self.addToggleOptions()
 		self.addPrevImgBox()
@@ -39,12 +44,11 @@ class MainPanel(wx.Panel):
 		self.addInputTxt()
 
 
-
 		self.topSizer.Add(self.toggleOptSizer)
 
-		self.imgBoxSizer.Add(self.prevImgBoxSizer)
-		self.imgBoxSizer.Add(self.currImgBoxSizer)
-		self.imgBoxSizer.Add(self.inputTxtSizer)
+		self.imgBoxSizer.Add(self.prevImgBoxSizer, flag=wx.ALIGN_CENTRE)
+		self.imgBoxSizer.Add(self.currImgBoxSizer, flag=wx.ALIGN_CENTRE)
+		self.imgBoxSizer.Add(self.inputTxtSizer, flag=wx.ALIGN_CENTRE)
 
 		self.topSizer.Add(self.imgBoxSizer, flag=wx.ALIGN_CENTRE)
 
@@ -56,6 +60,18 @@ class MainPanel(wx.Panel):
 
 		#self.parent.Bind(wx.EVT_CLOSE, self.closeHandler)
 
+
+	def loadSymDicts(self):
+		wordDict={}
+		temp={}
+
+		for file in self.defaultDict:
+			d=open( path.join(self.symDictPath,file) ).read().replace("\n","")
+			d=d.lower()
+			exec("temp="+"{"+d+"}")
+			wordDict.update(temp)
+
+		return wordDict
 
 	def addToggleOptions(self):
 		"""
@@ -81,8 +97,11 @@ class MainPanel(wx.Panel):
 		self.hideRomaji.SetToolTipString(tip)
 
 	def addPrevImgBox(self):
-		self.placeHolder= wx.TextCtrl(self, size=(500,100))
-		self.prevImgBoxSizer.Add(self.placeHolder, proportion=0, flag=wx.ALL, border=25)
+
+		imgFile=self.getImage(path.join(pathToModule,"symImg","ko.png"))
+		self.prevImgBoxSizer.Add(imgFile, proportion=0, flag=wx.ALL , border=25)
+
+
 
 	def addClickableOptions(self):
 		self.statsButton =wx.Button(self, label="View Statistics")
@@ -91,12 +110,28 @@ class MainPanel(wx.Panel):
 		self.clickableOptSizer.Add(self.statsButton ,proportion=0, flag=wx.ALL, border=20)
 		self.clickableOptSizer.Add(self.changeDict,proportion=0, flag=wx.ALL, border=20)
 
+	def getImage(self, filename):
+		print filename
+		img = wx.Image(filename, wx.BITMAP_TYPE_ANY).ConvertToBitmap()
+		imgRes=wx.StaticBitmap(self, -1, img, (0, 0), (img.GetWidth(), img.GetHeight()))
+
+		return imgRes
+
+	def drawImage(self, imageFileName, targSizer):
+		imgObj=self.getImage(path.join(self.symImgPath,imageFileName))
+		targSizer.Add(imgObj, proportion=0, flag=wx.ALL, border=0)
+
 	def addCurrImgBox(self):
-		self.currplaceHolder= wx.TextCtrl(self, size=(500,100))
-		self.currImgBoxSizer.Add(self.currplaceHolder, proportion=0, flag=wx.ALL, border=25)
+
+		randWord=randChoice(self.wordDict.keys())
+		self.wordDict.pop(randWord)
+		fileList=fileListForWord(randWord)
+		for filename in fileList:
+			self.drawImage(filename, self.currImgBoxSizer)
+
 
 	def addInputTxt(self):
-		self.inputTxt= wx.TextCtrl(self, size=(500,32))
+		self.inputTxt= wx.TextCtrl(self, size=(800,32))
 		self.inputTxt.SetValue("Enter syllables")
 
 		self.inputTxt.Bind(wx.EVT_LEFT_DOWN, handleClickInputBox, self.inputTxt)
@@ -109,7 +144,7 @@ class MainPanel(wx.Panel):
 
 class MainFrame(wx.Frame):
 	def __init__(self):
-		self.WindowSize=(875,400)
+		self.WindowSize=(1175,400)
 		wx.Frame.__init__(self, parent=None, id=wx.ID_ANY, title="Hiragana Memorization", size=self.WindowSize, style=wx.DEFAULT_FRAME_STYLE ^ wx.RESIZE_BORDER ^ wx.MAXIMIZE_BOX)#use default frame style but disable border resize and maximize
 
 		self.mp=MainPanel(self)
