eacf11188fc07e498bf252f734a734412d2d1fa0
Robert <robertCamba@gmail.com>
2015-06-23 19:43:03 -0500

    re:addToggleOptions()

---
+++ evtHandler.py
@@ -0,0 +1,101 @@
+import wx
+from random import choice as randChoice
+
+def hideDefinitionHandler(obj):
+	print "hideDefinitionHandler"
+
+def hideRomajiHandler(obj):
+	print "hideRomajiHandler"
+
+'''
+def handleClickInputBox(evt):
+	evt.GetEventObject().SetValue("")
+	evt.GetEventObject().SetFocus()
+
+
+def fileListForWord(word):
+	return [syllable.lower()+".png" for syllable in word.split('-')]
+
+def correctAnsForFileList(fileList):
+
+	word=""
+	vowels=['a','e','o','i','u']
+
+	for i in range (0,len(fileList)) :
+		fileList[i]=fileList[i].replace(".png","")
+
+		if fileList[i]=="minitsu":
+			if i<len(fileList)-1:
+				if fileList[i+1][0] not in vowels:
+					word=word+(fileList[i+1][0])
+				else:
+					raise ValueError("Last syllable must not be a vowel")
+			else:
+				raise ValueError("Last syllable can't be a Sokuon(little tsu)")
+
+		else:
+			word=word+(fileList[i])
+
+	return word
+
+def updatePrevImgBox(self, evt):
+	#self.prevImgSizer.Clear()
+	#self.prevImgNLabelSizer.Layout()
+	#self.imgBoxSizer.Layout()
+
+	fileList=fileListForWord(self.prevWord)
+	for filename in fileList:
+			self.drawImage(filename, self.prevImgSizer)
+	for i in self.prevImgHolder[:]:
+		i.Show()
+	self.prevImgSizer.Layout()
+	self.prevImgNLabelSizer.Layout()
+	self.imgBoxSizer.Layout()
+	self.Layout()
+
+
+def checkForCorrectAns(self, evt):
+	print evt.GetEventObject().GetValue()
+	from string import replace
+	eList=map(lambda x: x.replace(".png",""),fileListForWord(self.prevWord))
+	rStr="".join(eList)
+	print rStr
+	evt.GetEventObject().SetValue("")
+
+def handleInput(self,evt):
+	updatePrevImgBox(self, evt)
+	checkForCorrectAns(self, evt)
+	drawNextWord(self, evt)
+
+
+def drawNextWord(self, evt):
+
+		randWord=randChoice(self.wordDict.keys())
+		self.prevWord=randWord
+		self.definition=self.wordDict[randWord]
+		self.randWordLabel.SetLabel(self.definition)
+		self.wordDict.pop(randWord)
+		fileList=fileListForWord(randWord)
+
+		for i in self.imgHolder[:]:
+			i.Destroy()
+		self.imgHolder=[]
+
+		#self.currImgSizer.Clear()
+
+		#self.currImgNLabelSizer.Layout()
+		#self.imgBoxSizer.Layout()
+
+
+		for filename in fileList:
+			self.drawImage(filename, self.currImgSizer)
+
+
+		for i in self.imgHolder[:]:
+			i.Show()
+
+		#self.currImgNLabelSizer.Layout()
+		#self.imgBoxSizer.Layout()
+		self.Layout()
+
+'''
--- wxHiraganaMemorization.py
+++ wxHiraganaMemorization.py
@@ -1,13 +1,12 @@
 from ConfigParser import RawConfigParser
 from os import path
 import wx
-from hm_eventHandlers import *
-from random import choice as randChoice
+from evtHandler import *
+
 
 pathToModule=path.dirname(__file__)
 if len(pathToModule)==0:
 	pathToModule='.'
-
 
 
 class MainPanel(wx.Panel):
@@ -22,61 +21,54 @@ class MainPanel(wx.Panel):
 		self.imgBoxSize=(800,100)
 		self.symDictPath=path.join(pathToModule,"symDicts")
 		self.symImgPath=path.join(pathToModule,"symImg")
+		self.prevWord=""
+		self.definition=""
 
 		self.topSizer = wx.BoxSizer(wx.HORIZONTAL)
+
 		self.toggleOptSizer = wx.BoxSizer(wx.VERTICAL)
 
-		self.prevImgBoxSizer = wx.BoxSizer(wx.HORIZONTAL)
-		self.currImgBoxSizer= wx.BoxSizer(wx.HORIZONTAL)
+
+		self.prevImgSizer = wx.BoxSizer(wx.HORIZONTAL)
+		self.prevImgNLabelSizer = wx.BoxSizer(wx.VERTICAL)
+
+		self.currImgSizer = wx.BoxSizer(wx.HORIZONTAL)
+		self.currImgNLabelSizer = wx.BoxSizer(wx.VERTICAL)
+
 		self.imgBoxSizer= wx.BoxSizer(wx.VERTICAL)
 
-		self.clickableOptSizer= wx.BoxSizer(wx.VERTICAL)
+		self.clickableOptSizer = wx.BoxSizer(wx.VERTICAL)
 
 		self.inputTxtSizer = wx.BoxSizer(wx.HORIZONTAL)
 
-		self.defaultDict=["verbs"]
-		self.wordDict=self.loadSymDicts()
+		#self.imgHolder=[]
+		#self.prevImgHolder=[]
+		#self.defaultDict=["verbs"]
+		#self.wordDict=self.loadSymDicts()
 
 		self.addToggleOptions()
-		self.addPrevImgBox()
-		self.addClickableOptions()
-		self.addCurrImgBox()
-		self.addInputTxt()
+		#self.addPrevImgBox()
+		#self.addClickableOptions()
+		#self.addCurrImgBox()
+		#self.addInputTxt()
 
 
 		self.topSizer.Add(self.toggleOptSizer)
 
-		self.imgBoxSizer.Add(self.prevImgBoxSizer, flag=wx.ALIGN_CENTRE)
-		self.imgBoxSizer.Add(self.currImgBoxSizer, flag=wx.ALIGN_CENTRE)
+		self.imgBoxSizer.Add(self.prevImgNLabelSizer, flag=wx.ALIGN_CENTRE)
+		self.imgBoxSizer.Add(self.currImgNLabelSizer, flag=wx.ALIGN_CENTRE)
 		self.imgBoxSizer.Add(self.inputTxtSizer, flag=wx.ALIGN_CENTRE)
 
 		self.topSizer.Add(self.imgBoxSizer, flag=wx.ALIGN_CENTRE)
 
 		self.topSizer.Add(self.clickableOptSizer)
 
-
 		self.SetSizer(self.topSizer)
 		self.Layout()
 
 		#self.parent.Bind(wx.EVT_CLOSE, self.closeHandler)
 
-
-	def loadSymDicts(self):
-		wordDict={}
-		temp={}
-
-		for file in self.defaultDict:
-			d=open( path.join(self.symDictPath,file) ).read().replace("\n","")
-			d=d.lower()
-			exec("temp="+"{"+d+"}")
-			wordDict.update(temp)
-
-		return wordDict
-
 	def addToggleOptions(self):
-		"""
-
-		"""
 
 		self.hideRomaji= wx.CheckBox(parent=self, id=-1, label="Hide romaji")
 		self.hideDefinition= wx.CheckBox(parent=self, id=-1, label="Hide definition")
@@ -96,11 +88,13 @@ class MainPanel(wx.Panel):
 		tip="Hide phonetic pronounciation of the word"
 		self.hideRomaji.SetToolTipString(tip)
 
+	'''
 	def addPrevImgBox(self):
 
-		imgFile=self.getImage(path.join(pathToModule,"symImg","ko.png"))
-		self.prevImgBoxSizer.Add(imgFile, proportion=0, flag=wx.ALL , border=25)
-
+		imgFile=self.getImage(path.join(pathToModule,"symImg","ko.png"),True)
+		self.prevImgSizer.Add(imgFile, proportion=0, flag=wx.ALL)
+		self.prevImgNLabelSizer.Add(self.prevImgSizer)
+		#imgFile.Destroy()
 
 
 	def addClickableOptions(self):
@@ -110,37 +104,69 @@ class MainPanel(wx.Panel):
 		self.clickableOptSizer.Add(self.statsButton ,proportion=0, flag=wx.ALL, border=20)
 		self.clickableOptSizer.Add(self.changeDict,proportion=0, flag=wx.ALL, border=20)
 
-	def getImage(self, filename):
-		print filename
-		img = wx.Image(filename, wx.BITMAP_TYPE_ANY).ConvertToBitmap()
-		imgRes=wx.StaticBitmap(self, -1, img, (0, 0), (img.GetWidth(), img.GetHeight()))
 
+
+	def getImage(self, filename, n=False):
+		img = wx.Image(filename, wx.BITMAP_TYPE_PNG).ConvertToBitmap()
+
+		imgRes=wx.StaticBitmap(self, -1, img, (img.GetWidth(), img.GetHeight()))
+		if n==False:
+
+			imgRes.Hide()
+			self.imgHolder.append(imgRes)
+			self.prevImgHolder.append(wx.StaticBitmap(self, -1, img, (img.GetWidth(), img.GetHeight())))
 		return imgRes
 
 	def drawImage(self, imageFileName, targSizer):
+
 		imgObj=self.getImage(path.join(self.symImgPath,imageFileName))
+		#print dir(imgObj)
+
+
+
 		targSizer.Add(imgObj, proportion=0, flag=wx.ALL, border=0)
+
+
 
 	def addCurrImgBox(self):
 
 		randWord=randChoice(self.wordDict.keys())
+		self.prevWord=randWord
+		self.definition=self.wordDict[randWord]
+		self.randWordLabel=wx.StaticText(self)
+		self.randWordLabel.SetLabel(self.wordDict[randWord])
 		self.wordDict.pop(randWord)
 		fileList=fileListForWord(randWord)
+
+		font=wx.Font(14, wx.FONTFAMILY_DEFAULT, wx.FONTWEIGHT_NORMAL, wx.FONTWEIGHT_NORMAL)
+		self.randWordLabel.SetFont(font)
+		self.currImgNLabelSizer.Add(self.randWordLabel, flag=wx.ALIGN_CENTRE)
+
 		for filename in fileList:
-			self.drawImage(filename, self.currImgBoxSizer)
+			self.drawImage(filename, self.currImgSizer)
+
+		for i in self.imgHolder[:]:
+			i.Show()
+
+		self.currImgNLabelSizer.Add(self.currImgSizer)
+
 
 
 	def addInputTxt(self):
-		self.inputTxt= wx.TextCtrl(self, size=(800,32))
+		self.inputTxt= wx.TextCtrl(self, size=(800,32), style=wx.TE_PROCESS_ENTER)
 		self.inputTxt.SetValue("Enter syllables")
 
-		self.inputTxt.Bind(wx.EVT_LEFT_DOWN, handleClickInputBox, self.inputTxt)
+
+		self.inputTxt.Bind(wx.EVT_TEXT_ENTER, lambda evt :handleInput(self, evt) )
+		self.inputTxt.Bind(wx.EVT_LEFT_DOWN, handleClickInputBox)
 		self.inputTxt.Bind(wx.EVT_SET_FOCUS, handleClickInputBox)
 		self.inputTxt.Bind(wx.EVT_KILL_FOCUS, lambda evt : evt.GetEventObject().SetValue("Enter syllables"))
 
+		self.inputTxt.SetFocus()
+
 		self.inputTxtSizer.Add(self.inputTxt, proportion=0, flag=wx.ALL, border=25)
 
-
+	'''
 
 class MainFrame(wx.Frame):
 	def __init__(self):
