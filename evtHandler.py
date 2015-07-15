4689b44182d90b4929c2a7f892702aa9da449010
Robert <robertCamba@gmail.com>
2015-07-14 23:34:05 -0500

    renaming var+functions and adding placeholder functions

--- evtHandler.py
+++ evtHandler.py
@@ -1,19 +1,42 @@
 import wx
+import re
 from random import choice as randChoice
 
+def insertDictHandler(self, evt):
+	print "insertDictBtnHandler"
 
-def searchBarHandler(self, evt):
+def removeDictHandler(self, evt):
+	print "removeDictHandler"
 
-	if evt.GetEventObject().GetValue() in self.dictList:
+def curr_sb_handler(self, evt):
+	pass
+
+def curr_dlb_handler(self, evt):
+	pass
+
+def unused_dlb_handler(self, evt):
+	print evt.GetEventObject().GetSelections()
+
+def unused_sb_handler(self, evt):
+
+	if evt.GetEventObject().GetValue() in self.unusedDicts:
 		#pass
-		self.searchBar.SetBackgroundColour((0,255,0))
-		self.searchBar.SetForegroundColour((0,0,0))#text color
+
+
+		self.unused_dict_search_bar.SetBackgroundColour((0,255,0))
+		self.unused_dict_search_bar.SetForegroundColour((0,0,0))#text color
 
 	else:
 		#pass
-		self.searchBar.SetBackgroundColour((255,0,0))
-		self.searchBar.SetForegroundColour((255,255,255))#text color
-		self.Refresh()
+		self.unused_dict_search_bar.SetBackgroundColour((255,0,0))
+		self.unused_dict_search_bar.SetForegroundColour((255,255,255))#text color
+
+	item_list = self.unusedDicts
+
+	pattern = re.compile(evt.GetEventObject().GetValue())
+	self.unused_dict_box.Set([item  for item in item_list if re.search(pattern, item) is not None])
+
+	self.Refresh()
 
 
 def statsBtnHandler(evt):
@@ -21,9 +44,9 @@ def statsBtnHandler(evt):
 
 def changesDictBtnHandler(self, evt):
 	print "Change dict!"
-	self.parent.cdp.searchBar.SetValue("")
-	self.parent.cdp.Center()
-	self.parent.cdp.Show()
+	self.cdp.unused_dict_search_bar.SetValue("")
+	self.cdp.Center()
+	self.cdp.Show()
 
 
 def resetToggleOpt(self, evt, resetTargs=["def","sylImg"]):
