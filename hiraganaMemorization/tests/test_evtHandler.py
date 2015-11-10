a54dd063190d25bc4fc52f97ee7c982cc6a952b1
Robert Camba <umcamba@myumanitoba.ca>
2015-11-10 10:56:45 -0600

    ~pep8, renaming methods

--- hiraganaMemorization/tests/test_evtHandler.py
+++ hiraganaMemorization/tests/test_evtHandler.py
@@ -1,40 +1,42 @@
 import unittest
 from sys import path
+
 path.append("../")
 from evtHandler import generateCorrectAns
 
+
 class TestEvts(unittest.TestCase):
+
 	def setUp(self):
 		print "\nStarting ", self._testMethodName
 
-	def test_wordForFileList_valid(self):
-		word="sa-sokuon-ki"
+	def test_generateCorrectAns_valid(self):
+		word = "sa-sokuon-ki"
 
-		correctAns=generateCorrectAns(word)
+		correctAns = generateCorrectAns(word)
 		self.assertEqual(correctAns, "sakki")
 
-
-	def test_wordForFileList_invalid_sokuon(self):
-		word="sokuon-sokuon"
-		expectedMsg="Last syllable can't be a sokuon"
-
-		with self.assertRaises(ValueError) as context:
-			generateCorrectAns(word)
-
-		self.assertIn(expectedMsg, str(context.exception))
-
-	def test_wordForFileList_invalid_lastWordSokuon(self):
-		word="ka-ki-sokuon"
-		expectedMsg="Last syllable can't be a sokuon"
+	def test_generateCorrectAns_invalid_sokuon(self):
+		word = "sokuon-sokuon"
+		expectedMsg = "Last syllable can't be a sokuon"
 
 		with self.assertRaises(ValueError) as context:
 			generateCorrectAns(word)
 
 		self.assertIn(expectedMsg, str(context.exception))
 
-	def test_wordForFileList_invalid_lastWordVowel(self):
-		word="sokuon-o"
-		expectedMsg="must not be a vowel"
+	def test_generateCorrectAns_invalid_lastWordSokuon(self):
+		word = "ka-ki-sokuon"
+		expectedMsg = "Last syllable can't be a sokuon"
+
+		with self.assertRaises(ValueError) as context:
+			generateCorrectAns(word)
+
+		self.assertIn(expectedMsg, str(context.exception))
+
+	def test_generateCorrectAns_invalid_lastWordVowel(self):
+		word = "sokuon-o"
+		expectedMsg = "must not be a vowel"
 
 		with self.assertRaises(ValueError) as context:
 			generateCorrectAns(word)
@@ -42,25 +44,24 @@ class TestEvts(unittest.TestCase):
 		self.assertIn(expectedMsg, str(context.exception))
 
 
-def getHMEvts_TS():
-	ts= unittest.TestSuite()
+def getEvts_TS():
+	ts = unittest.TestSuite()
 
-	ts.addTest( TestEvts( "test_wordForFileList_valid"))
-	ts.addTest( TestEvts( "test_wordForFileList_invalid_sokuon"))
-	ts.addTest( TestEvts( "test_wordForFileList_invalid_lastWordSokuon"))
-	ts.addTest( TestEvts( "test_wordForFileList_invalid_lastWordVowel"))
-
+	ts.addTest(TestEvts("test_generateCorrectAns_valid"))
+	ts.addTest(TestEvts("test_generateCorrectAns_invalid_sokuon"))
+	ts.addTest(TestEvts("test_generateCorrectAns_invalid_lastWordSokuon"))
+	ts.addTest(TestEvts("test_generateCorrectAns_invalid_lastWordVowel"))
 
 	return ts
 
-if __name__=="__main__":
 
+if __name__ == "__main__":
 
-	suiteList=[]
+	suiteList = []
 
-	hmEvts_TS=getHMEvts_TS()
+	evts_TS = getEvts_TS()
 
-	suiteList.append(hmEvts_TS)
+	suiteList.append(evts_TS)
 	fullSuite = unittest.TestSuite(suiteList)
 
 	runner = unittest.TextTestRunner()
