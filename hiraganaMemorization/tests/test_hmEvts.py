95c09f5e48b05079764e40085314535d7d78a43a
Robert <robertCamba@gmail.com>
2015-11-10 00:34:59 -0600

    update init test; test for generateCorrectAns

--- hiraganaMemorization/tests/test_hmEvts.py
+++ hiraganaMemorization/tests/test_hmEvts.py
@@ -1,58 +1,54 @@
 import unittest
 from sys import path
 path.append("../")
-from hm_eventHandlers import fileListForWord, correctAnsForFileList
+from evtHandler import generateCorrectAns
 
-class TestHMEvts(unittest.TestCase):
+class TestEvts(unittest.TestCase):
 	def setUp(self):
 		print "\nStarting ", self._testMethodName
 
 	def test_wordForFileList_valid(self):
-		word="sa-minitsu-ki"
-		fileList=fileListForWord(word)
+		word="sa-sokuon-ki"
 
-		correctAns=correctAnsForFileList(fileList)
+		correctAns=generateCorrectAns(word)
 		self.assertEqual(correctAns, "sakki")
 
 
-	def test_wordForFileList_invalid_minitsu(self):
-		word="minitsu-minitsu"
-		fileList=fileListForWord(word)
-		expectedMsg="Last syllable can't be a Sokuon"
+	def test_wordForFileList_invalid_sokuon(self):
+		word="sokuon-sokuon"
+		expectedMsg="Last syllable can't be a sokuon"
 
 		with self.assertRaises(ValueError) as context:
-			correctAnsForFileList(fileList)
+			generateCorrectAns(word)
 
-		self.assertTrue( expectedMsg in str(context.exception) )
+		self.assertIn(expectedMsg, str(context.exception))
 
-	def test_wordForFileList_invalid_lastWordMinitsu(self):
-		word="ka-ki-minitsu"
-		fileList=fileListForWord(word)
-		expectedMsg="Last syllable can't be a Sokuon"
+	def test_wordForFileList_invalid_lastWordSokuon(self):
+		word="ka-ki-sokuon"
+		expectedMsg="Last syllable can't be a sokuon"
 
 		with self.assertRaises(ValueError) as context:
-			correctAnsForFileList(fileList)
+			generateCorrectAns(word)
 
-		self.assertTrue( expectedMsg in str(context.exception) )
+		self.assertIn(expectedMsg, str(context.exception))
 
 	def test_wordForFileList_invalid_lastWordVowel(self):
-		word="minitsu-o"
-		fileList=fileListForWord(word)
-		expectedMsg="Last syllable must not be a vowel"
+		word="sokuon-o"
+		expectedMsg="must not be a vowel"
 
 		with self.assertRaises(ValueError) as context:
-			correctAnsForFileList(fileList)
+			generateCorrectAns(word)
 
-		self.assertTrue( expectedMsg in str(context.exception) )
+		self.assertIn(expectedMsg, str(context.exception))
 
 
 def getHMEvts_TS():
 	ts= unittest.TestSuite()
 
-	ts.addTest( TestHMEvts( "test_wordForFileList_valid"))
-	ts.addTest( TestHMEvts( "test_wordForFileList_invalid_minitsu"))
-	ts.addTest( TestHMEvts( "test_wordForFileList_invalid_lastWordMinitsu"))
-	ts.addTest( TestHMEvts( "test_wordForFileList_invalid_lastWordVowel"))
+	ts.addTest( TestEvts( "test_wordForFileList_valid"))
+	ts.addTest( TestEvts( "test_wordForFileList_invalid_sokuon"))
+	ts.addTest( TestEvts( "test_wordForFileList_invalid_lastWordSokuon"))
+	ts.addTest( TestEvts( "test_wordForFileList_invalid_lastWordVowel"))
 
 
 	return ts
